15d24835ff6e9438e1fd5d13dbdd42b94741df7f
Larimar Fondeur <larimarfondeur@gmail.com>
2022-06-29 11:30:58 -0400

    changed styles and color and tex

---
+++ main.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "My mother"
+document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "A free spirit"
+document.querySelector("body").appendChild(h3);
